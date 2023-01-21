3213fb1751a24dfb50244ebc032a9649443965c2
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-22 03:54:03 +0500

    file Naming Convention

--- scratch/src/main.ts
+++ scratch/src/main.ts
@@ -1,18 +1,5 @@
-import { Controller, Get, Module } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
-@Controller()
-class AppController {
-	@Get()
-	findAll(): string {
-		return 'This action returns all cats';
-	}
-}
-
-@Module({
-	controllers: [AppController],
-})
-class AppModule {}
+import { AppModule } from './app.module';
 
 const bootstrap = async () => {
 	const app = await NestFactory.create(AppModule);
